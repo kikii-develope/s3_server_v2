619c87897f1d72d50fd6cd5524da73bac4e255ac
kiki-InseokLee <tjrkd222@kikii.com>
2025-06-18 14:59:48 +0900

    파일명 인코딩 테스트 완료(모바일 웹, 안드로이드)

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ import 'dotenv/config.js'
 
 const app = express();
 
+const upload = multer({ storage: memoryStorage(), preservePath: false });
+
 // 요청 패킷 정보를 로깅하는 미들웨어
 const requestLogger = (req, res, next) => {
     console.log('\n=== 요청 패킷 정보 ===');
@@ -22,11 +24,11 @@ const requestLogger = (req, res, next) => {
 };
 
 app.use(cors());
+
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
 app.use(requestLogger);  // 로깅 미들웨어 추가
-
-const upload = multer({ storage: memoryStorage() });
 
 app.get('/', (req, res) => {
     res.send('Hello World');
@@ -52,9 +54,15 @@ app.post('s3/upload', upload.single('file'), async (req, res) => {
         return res.status(200).json({ message: '파일 업로드 성공', status: 200, object: result });
     } catch (error) {
         console.error('업로드 에러:', error);
-        res.status(500).json({ message: error.message, status: 500 });
+        res.status(400).json({ message: error.message, status: 400 });
     }
 });
+
+app.listen(8989, () => {
+    console.log('Server is running on port 8989');
+    console.log("app version: ", process.env.APP_VERSION);
+});
+
 
 app.post('/s3/upload/multiple', upload.array('files', 10), async (req, res) => {
     // console.log(req);
@@ -68,6 +76,7 @@ app.post('/s3/upload/multiple', upload.array('files', 10), async (req, res) => {
     }
     
     const { bucketName, path } = req.body;
+    console.log(bucketName, path);
 
     if(!bucketName) {
         return res.status(400).json({ message: 'bucketName is missing in request body.', status: 400 });
@@ -87,11 +96,6 @@ app.post('/s3/upload/multiple', upload.array('files', 10), async (req, res) => {
         return res.status(200).json({ message: '파일 업로드 성공', status: 200, object: result });
     } catch (error) {
         console.error('멀티 업로드 에러:', error);
-        res.status(500).json({ message: error.message, status: 500 });
+        res.status(400).json({ message: error.message, status: 400 });
     }
-});
-
-app.listen(8989, () => {
-    console.log('Server is running on port 8989');
-    console.log("app version: ", process.env.APP_VERSION);
 });
