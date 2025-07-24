4f2efa5015cda771c7d53c84f26280b8418f873e
jsh255 <jsh@kikii.com>
2025-07-24 15:51:23 +0900

    s3 환경변수 이름 수정

--- src/s3_client.js
+++ src/s3_client.js
@@ -3,16 +3,17 @@ import 'dotenv/config.js'
 
 console.log('AWS 환경 변수 확인:', {
     region: process.env.AWS_REGION,
-    hasAccessKey: !!process.env.AWS_IAM_USER_KEY,
-    hasSecretKey: !!process.env.AWS_IAM_USER_SECRET
+    hasAccessKey: !!process.env.AWS_ACCESS_KEY_ID,
+    hasSecretKey: !!process.env.AWS_SECRET_ACCESS_KEY
 });
 
 const s3Client = new S3Client({
     region: process.env.AWS_REGION || 'ap-northeast-2',
     credentials: {
-        accessKeyId: process.env.AWS_IAM_USER_KEY || '',
-        secretAccessKey: process.env.AWS_IAM_USER_SECRET || ''
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || ''
     }
 });
+
 
 export default s3Client;
